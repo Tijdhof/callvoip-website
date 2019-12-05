5cd545adab68ec72159d453a852321759c7ba146
Frank Spin <info@frankspin.nl>
2019-12-05 10:33:11 +0100

    file input

--- themes/callvoiptelefonie/assets/js/main.js
+++ themes/callvoiptelefonie/assets/js/main.js
@@ -383,3 +383,31 @@ function pageSummary() {
 pageSummary();
 
 
+
+var inputs = document.querySelectorAll( ".inputfile");
+Array.prototype.forEach.call( inputs, function( input )
+{
+	var label	 = input.nextElementSibling,
+    labelVal = label.innerHTML;
+    
+  console.log('label', label)
+
+	input.addEventListener( "change", function( e )
+	{
+		var fileName = "";
+		if( this.files && this.files.length > 1 ) {
+      fileName = ( this.getAttribute( "data-multiple-caption" ) || "" ).replace( "{count}", this.files.length );
+    }
+		else {
+      fileName = e.target.value.split(" \ ").pop()
+    }
+
+    if( fileName ) {
+      fileName = fileName.replace(/^.*\\/, "");
+      label.querySelector("span").innerHTML = fileName;
+    }
+		else {
+      label.innerHTML = labelVal;
+    }
+	});
+});
