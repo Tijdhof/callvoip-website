a5338a45251d92b68bcf297123885642bdd37b2b
TimTijdhof <53812393+TimTijdhof@users.noreply.github.com>
2021-09-14 11:44:19 +0200

    Update calculator.js

--- themes/callvoiptelefonie/assets/js/calculator.js
+++ themes/callvoiptelefonie/assets/js/calculator.js
@@ -165,7 +165,7 @@ const renderBasicFields = function (fields) {
     obj.value = item.value;
 
     monthly = monthly + obj.price_monthly * obj.value;
-    onetime = obj.value > 0 ? obj.price_onetime * 1 : 0;
+    onetime = onetime + obj.price_onetime * obj.value;
 
     state.basicFields.push(obj);
   }
@@ -389,7 +389,7 @@ const renderCrmFields = function (fields) {
     obj.value = item.value;
 
     monthly = monthly + obj.price_monthly * obj.value;
-    onetime = monthly + obj.price_onetime * obj.value;
+    onetime = obj.value > 0 ? obj.price_onetime * 1 : 0;
 
     state.crmFields.push(obj);
   }
